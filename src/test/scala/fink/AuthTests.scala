beed1749f5a100f81a5b775c6e02877de9b74aea
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2020-09-27 19:09:40 +0200

    Fixed auth specs

--- src/test/scala/fink/AuthTests.scala
+++ src/test/scala/fink/AuthTests.scala
@@ -2,30 +2,31 @@ package fink
 
 import java.time.Instant
 
-import pdi.jwt.{Jwt, JwtAlgorithm, JwtCirce, JwtClaim}
+import org.specs2.mutable.Specification
+import pdi.jwt.{JwtAlgorithm, JwtCirce, JwtClaim}
 
-import cats.implicits._
+class AuthTests extends Specification {
 
-object AuthTests extends App {
+  "Should decode JWT" in {
 
-  val claim = JwtClaim(
-    issuedAt = Some(Instant.now.getEpochSecond),
-    content = """{"authUserId":42}"""
-  )
+    val claim = JwtClaim(
+      issuedAt = Some(Instant.now.getEpochSecond),
+      content = """{"authUserId":42}"""
+    )
 
-  val key = "secretK3y"
-  val algo = JwtAlgorithm.HS256
-  val token = JwtCirce.encode(claim, key, algo)
+    val key = "secretK3y"
+    val algo = JwtAlgorithm.HS256
+    val token = JwtCirce.encode(claim, key, algo)
 
-  println(token)
+    println(token)
 
-  val res = JwtCirce.decodeJson(token, key, Seq(JwtAlgorithm.HS256)).toEither
-  println(res)
+    val tokenJson = JwtCirce.decodeJson(token, key, Seq(JwtAlgorithm.HS256)).toEither.getOrElse(sys.error(""))
 
-  val res1 = res.fold(_ => (), _.hcursor.downField("authUserId").as[Long])
-  println(res1)
+    val authUserId = tokenJson.hcursor.downField("authUserId").as[Long].getOrElse(sys.error(""))
+    authUserId should_== 42
 
-  // Jwt.validate()
+    // Jwt.validate()
 
+  }
 
 }
