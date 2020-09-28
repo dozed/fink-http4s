645f13f06e767bafcccf4170a4b0b05abceac0bf
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2020-09-28 19:41:43 +0200

    Renamed field

--- frontend/src/admin/AdminApp.js
+++ frontend/src/admin/AdminApp.js
@@ -19,8 +19,8 @@ class AdminLogin extends Component {
     return (
       <form className="form-signin text-center">
         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label htmlFor="inputEmail" className="sr-only">Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required=""  autoFocus="" onChange={this.setUsername} />
+        <label htmlFor="inputUsername" className="sr-only">Username</label>
+        <input type="text" id="inputUsername" className="form-control" placeholder="Username" required=""  autoFocus="" onChange={this.setUsername} />
         <label htmlFor="inputPassword" className="sr-only">Password</label>
         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required="" onChange={this.setPassword} />
         <button className="btn btn-lg btn-primary btn-block" type="button" onClick={() => this.props.onLogin(this.state.username, this.state.password)}>Sign in</button>
