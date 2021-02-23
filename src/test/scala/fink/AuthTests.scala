08c135c35f44de9024623c18f95c5623c3fe6097
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-02-23 20:08:06 +0100

    Auth tests

--- src/test/scala/fink/AuthTests.scala
+++ src/test/scala/fink/AuthTests.scala
@@ -2,30 +2,64 @@ package fink
 
 import java.time.Instant
 
+import cats.effect.IO
+import fink.data.AppConfig
+import fink.modules.AuthModule
+import org.http4s._
+import org.specs2.matcher.ThrownMessages
 import org.specs2.mutable.Specification
 import pdi.jwt.{JwtAlgorithm, JwtCirce, JwtClaim}
 
-class AuthTests extends Specification {
+class AuthTests extends Specification with ThrownMessages {
 
   "Should decode JWT" in {
 
-    val claim = JwtClaim(
-      issuedAt = Some(Instant.now.getEpochSecond),
-      content = """{"authUserId":42}"""
-    )
-
+    val userId = 42
     val key = "secretK3y"
     val algo = JwtAlgorithm.HS256
+    val now = Instant.now.getEpochSecond
+
+    val claim = JwtClaim(
+      issuedAt = Some(now),
+      content = f"""{"authUserId":$userId}"""
+    )
+
     val token = JwtCirce.encode(claim, key, algo)
 
-    println(token)
+    val tokenJson = JwtCirce.decodeJson(token, key, List(algo)).getOrElse(fail("Error decoding token"))
 
-    val tokenJson = JwtCirce.decodeJson(token, key, Seq(JwtAlgorithm.HS256)).toEither.getOrElse(sys.error(""))
+    val authUserId = tokenJson.hcursor.get[Long]("authUserId").getOrElse(fail("Error reading authUserId from token"))
+    authUserId should_== userId
 
-    val authUserId = tokenJson.hcursor.downField("authUserId").as[Long].getOrElse(sys.error(""))
-    authUserId should_== 42
+    val issuedAt = tokenJson.hcursor.get[Long]("iat").getOrElse(fail("Error reading iat from token"))
+    issuedAt should_== now
 
-    // Jwt.validate()
+  }
+
+  "Should read userId from Request" in {
+
+    World.config = AppConfig.load()
+
+    val userId = 42
+    val key = "secretK3y"
+    val algo = JwtAlgorithm.HS256
+    val now = Instant.now.getEpochSecond
+
+    val claim = JwtClaim(
+      issuedAt = Some(now),
+      content = f"""{"authUserId":$userId}"""
+    )
+
+    val token = JwtCirce.encode(claim, key, algo)
+
+    val req = Request[IO](Method.GET)
+      .putHeaders(
+        headers.Cookie(RequestCookie("ui", token))
+      )
+
+    val res = AuthModule.readUserId(req)
+
+    res should_== Right(42)
 
   }
 
