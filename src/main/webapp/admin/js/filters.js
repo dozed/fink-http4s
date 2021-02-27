32c0ee5f4303472381c69e1dfcbe73c65deec45f
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-02-27 15:43:57 +0100

    Removed old webapp

--- src/main/webapp/admin/js/filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('fink.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
