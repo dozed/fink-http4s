4210cd170c81644cf89d5e12cbf4bd078d490913
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-02-27 19:07:42 +0100

    Authorization tests

--- src/test/scala/fink/AuthorizationTests.scala
+++ src/test/scala/fink/AuthorizationTests.scala
@@ -1,24 +1,11 @@
 package fink
 
-import cats.effect.IO
-import doobie.Transactor
-import doobie.implicits._
-import fink.World.{config, cs, xa}
 import fink.auth.Authorization
-import fink.data.{AppConfig, ErrorCode, User, UserRole}
-import fink.db.DbSetup
+import fink.data.{ErrorCode, User, UserRole}
 import org.specs2.matcher.ThrownMessages
 import org.specs2.mutable.Specification
 
 class AuthorizationTests extends Specification with ThrownMessages {
-
-  World.config = AppConfig.load()
-
-  World.xa = Transactor.fromDriverManager[IO](
-    config.dbConfig.driver, config.dbConfig.db, config.dbConfig.user, config.dbConfig.password
-  )
-
-  DbSetup.setupDb.transact(xa).unsafeRunSync
 
   val testUserWithEditRole = User(2, "foo", "bar", Set(UserRole.CanEdit))
   val testUserWithoutRoles = User(2, "foo", "bar", Set.empty)
