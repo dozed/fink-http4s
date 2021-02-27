2cb861416e69b7079f0067763a27fcb377c490ef
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-02-27 15:29:12 +0100

    Use recoverWith

--- src/main/scala/fink/util/ErrorHandling.scala
+++ src/main/scala/fink/util/ErrorHandling.scala
@@ -1,6 +1,6 @@
 package fink.util
 
-import cats.ApplicativeThrow
+import cats.{ApplicativeError, ApplicativeThrow, Apply}
 import cats.data.Kleisli
 import cats.syntax.applicativeError._
 import org.http4s.{Http, Response}
@@ -9,7 +9,16 @@ object ErrorHandling {
   def apply[F[_], G[_]](http: Http[F, G], errorHandler: PartialFunction[Throwable, F[Response[G]]])
     (implicit F: ApplicativeThrow[F]): Http[F, G] =
     Kleisli { req =>
-      http(req).handleErrorWith(e => errorHandler.applyOrElse(e, F.raiseError))
+      http(req).recoverWith(errorHandler)
     }
 }
 
+trait Foo[F[_]] extends ApplicativeError[F, Throwable] {
+
+  def onError1[A](fa: F[A])(pf: PartialFunction[Throwable, F[Unit]]): F[A] =
+    handleErrorWith(fa) { e =>
+      pf.andThen(fu => map2(fu, raiseError[A](e))((_, b) => b))
+        .applyOrElse(e, raiseError[A])
+    }
+
+}
