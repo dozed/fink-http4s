6e105933a79f1b2843e60efb367e83a5635f8274
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-02-28 19:05:43 +0100

    Login with enter key

--- frontend-editor/src/EditorLogin.js
+++ frontend-editor/src/EditorLogin.js
@@ -16,9 +16,9 @@ export default class EditorLogin extends Component {
       <form className="form-signin text-center">
         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label htmlFor="inputUsername" className="sr-only">Username</label>
-        <input type="text" id="inputUsername" className="form-control" placeholder="Username" required=""  autoFocus="" onChange={this.setUsername} value={this.state.username} />
+        <input type="text" id="inputUsername" className="form-control" placeholder="Username" required=""  autoFocus="" onChange={this.setUsername} value={this.state.username} onKeyUp={this.handleKeyPress} />
         <label htmlFor="inputPassword" className="sr-only">Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required="" onChange={this.setPassword} value={this.state.password} />
+        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required="" onChange={this.setPassword} value={this.state.password} onKeyUp={this.handleKeyPress} />
         <button className="btn btn-lg btn-primary btn-block" type="button" onClick={() => this.props.onLogin(this.state.username, this.state.password)}>Sign in</button>
         { this.props.errorMessage && <div>{this.props.errorMessage}</div>}
       </form>
@@ -35,6 +35,12 @@ export default class EditorLogin extends Component {
     this.setState({
       password: e.target.value
     });
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.props.onLogin(this.state.username, this.state.password)
+    }
   }
 
 }
