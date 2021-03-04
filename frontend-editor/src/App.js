4b008696303db84f32167980a950b6633b628185
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-03-04 17:46:46 +0100

    Merged EditorIndex into App

--- frontend-editor/src/App.js
+++ frontend-editor/src/App.js
@@ -1,7 +1,20 @@
 import React, {Component} from "react";
 import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-import EditorIndex from "EditorIndex";
+import Galleries from "Galleries";
+import CreateGallery from "CreateGallery";
+import EditGallery from "EditGallery";
+import Posts from "Posts";
+import CreatePost from "CreatePost";
+import EditPost from "EditPost";
+import Pages from "Pages";
+import CreatePage from "CreatePage";
+import EditPage from "EditPage";
 import Login from "Login";
 import {fetchMe, login, logout} from "../../frontend-shared/api";
 
@@ -25,8 +38,47 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        {!this.state.loading && this.state.user && <EditorIndex onLogout={this.logout} />}
         {!this.state.loading && !this.state.user && <Login onLogin={this.login} errorMessage={this.state.loginErrorMessage} />}
+        {!this.state.loading && this.state.user &&
+          <Router>
+            <Navbar bg="light" expand="lg">
+              <LinkContainer to="/">
+                <Navbar.Brand>fink</Navbar.Brand>
+              </LinkContainer>
+              <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <LinkContainer to="/posts">
+                    <Nav.Link>Posts</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/pages">
+                    <Nav.Link>Pages</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/galleries">
+                    <Nav.Link>Galleries</Nav.Link>
+                  </LinkContainer>
+                </Nav>
+              </Navbar.Collapse>
+              <Navbar.Brand className="pull-right">
+                <button className="btn btn-primary" onClick={() => this.logout()}>Logout</button>
+              </Navbar.Brand>
+            </Navbar>
+            <Container fluid={true}>
+              <Switch>
+                {/*<Route path="/" exact component={Home} />*/}
+                <Route path="/galleries" exact component={Galleries} />
+                <Route path="/galleries/create" exact component={CreateGallery} />
+                <Route path="/galleries/:galleryId" component={EditGallery} />
+                <Route path="/posts" exact component={Posts} />
+                <Route path="/posts/create" exact component={CreatePost} />
+                <Route path="/posts/:postId" component={EditPost} />
+                <Route path="/pages" exact component={Pages} />
+                <Route path="/pages/create" exact component={CreatePage} />
+                <Route path="/pages/:pageId" component={EditPage} />
+              </Switch>
+            </Container>
+          </Router>
+        }
       </div>
     );
   }
