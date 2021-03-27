745cd1395d90d28bb5b520c8ebdb2f1054acb48f
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2021-03-27 16:47:54 +0100

    Use context bound

--- src/main/scala/fink/util/ErrorHandling.scala
+++ src/main/scala/fink/util/ErrorHandling.scala
@@ -6,8 +6,10 @@ import cats.syntax.applicativeError._
 import org.http4s.{Http, Request, Response}
 
 object ErrorHandling {
-  def apply[F[_], G[_]](http: Http[F, G], errorHandler: Request[G] => PartialFunction[Throwable, F[Response[G]]])
-    (implicit F: ApplicativeThrow[F]): Http[F, G] =
+  def apply[F[_]: ApplicativeThrow, G[_]](
+    http: Http[F, G],
+    errorHandler: Request[G] => PartialFunction[Throwable, F[Response[G]]]
+  ): Http[F, G] =
     Kleisli { req =>
       http(req).recoverWith(errorHandler(req))
     }
